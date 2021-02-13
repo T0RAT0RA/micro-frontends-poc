6140ef8e6ab6f6936ce5c8b9a074c72d18a7a518
T0RAT0RA <stephane.reiss@gmail.com>
2021-02-13 15:40:36 -0500

    Trying frintjs

--- clients/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="">
-        ACCOUNTING
-        <router-view></router-view>
-    </div>
-</template>
--- src/config.js
+++
@@ -1,8 +0,0 @@
-import * as isActive from './activityFns.js'
-import * as singleSpa from 'single-spa'
-
-singleSpa.registerApplication('sidebar', () => SystemJS.import('@apps/sidebar'), isActive.sidebar)
-singleSpa.registerApplication('header', () => SystemJS.import('@apps/header'), isActive.header)
-singleSpa.registerApplication('clients', () => SystemJS.import('@apps/clients'), isActive.clients)
-
-singleSpa.start()
